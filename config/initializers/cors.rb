6b82f84f8473e39f970d022cff694fe93135cbc0
scottasc <scottschulfer@gmail.com>
2018-11-29 08:10:04 -0600

    whitelisting heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,4 +6,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  allow do
+    origins 'powerful-plateau-48096.herokuapp.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+  
 end
